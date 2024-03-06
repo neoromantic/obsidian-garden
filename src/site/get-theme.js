f972d10e2e902274e793bbe838aca6938a255105
Sergey Petrov <sergey@goodit.works>
2024-03-06 14:01:27 +0700

    Новый get-theme

--- src/site/get-theme.js
+++ src/site/get-theme.js
@@ -8,9 +8,12 @@ const themeCommentRegex = /\/\*[\s\S]*?\*\//g;
 
 async function getTheme() {
   let themeUrl = process.env.THEME;
+  console.log(`Current THEME is set to ${themeUrl}`)
   if (themeUrl) {
     //https://forum.obsidian.md/t/1-0-theme-migration-guide/42537
     //Not all themes with no legacy mark have a theme.css file, so we need to check for it
+
+    console.log("  theme: checking for theme.css..")
     try {
       await axios.get(themeUrl);
     } catch {
@@ -20,14 +23,23 @@ async function getTheme() {
         themeUrl = themeUrl.replace("obsidian.css", "theme.css");
       }
     }
+    console.log(`  theme: final URL is ${themeUrl}`)
 
     const res = await axios.get(themeUrl);
+    console.log(`  theme: axios GET request made to ${themeUrl}`);
+    console.log(`  theme: response status is ${res.status}`);
+    console.log(`  theme: response status text is ${res.statusText}`);
+    console.log(`  theme: response headers are ${JSON.stringify(res.headers)}`);
+    console.log(`  theme: response data type is ${typeof res.data}`);
     try {
       const existing = globSync("src/site/styles/_theme.*.css");
       existing.forEach((file) => {
         fs.rmSync(file);
       });
-    } catch {}
+    } catch (err) {
+      console.error(`  theme: error while deleting existing theme files: ${err.message}`);
+      console.log(`  theme: error details: ${JSON.stringify(err, null, 2)}`);
+    }
     let skippedFirstComment = false;
     const data = res.data.replace(themeCommentRegex, (match) => {
       if (skippedFirstComment) {
@@ -40,7 +52,9 @@ async function getTheme() {
     const hashSum = crypto.createHash("sha256");
     hashSum.update(data);
     const hex = hashSum.digest("hex");
-    fs.writeFileSync(`src/site/styles/_theme.${hex.substring(0, 8)}.css`, data);
+    const filename = `src/site/styles/_theme.${hex.substring(0, 8)}.css`
+    console.log(`  theme: writing theme file as ${filename}`)
+    fs.writeFileSync(filename, data);
   }
 }
 
